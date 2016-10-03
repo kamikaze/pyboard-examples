b8f8e8116fc3d13e86b9e898987b834e221e7b98
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2016-10-03 05:26:21 +0300

    working on GPS/UART examples

---
+++ gps.py
@@ -0,0 +1,23 @@
+import os
+from pyb import delay, UART
+
+
+def log_gps(uart, read_delay=100):
+    with open('/sd/gps.log', 'w') as f:
+        try:
+            while True:
+                while uart.any():
+                    line = uart.readline()
+                    print(line, file=f)
+                    #print(line)
+            delay(read_delay)
+        except KeyboardInterrupt:
+            pass
+
+    os.sync()
+
+
+def run_gps_logger(uart_id, baudrate=9600):
+    uart = UART(uart_id, baudrate)
+    log_gps(uart, 10)
+
