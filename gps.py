db5127a05e18a134755ee1ca672fcfb185b82c3d
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2016-10-05 05:47:17 +0300

    working on GPS/UART examples

--- gps.py
+++ gps.py
@@ -1,21 +1,42 @@
 import os
-from pyb import delay, UART
+from pyb import wfi, UART, LED
+
+
+def parse(line):
+    sentence, *data, checksum = line.split(',')
+
+    if sentence == '$GPGGA':
+        t, lat, lat_part, lng, lng_part, fix, sat_cnt, hdil, alt, _, geoid_height, _, _  = data
+
+        if fix == '0':
+            LED(2).off()
+            LED(3).on()
+        else:
+            LED(3).off()
+            LED(2).on()
 
 
 def log_gps(uart, read_delay=100):
+    led = LED(4)
     with open('/sd/gps.log', 'w') as f:
         try:
             while True:
                 while uart.any():
-                    line = uart.readline()
+                    line = str(uart.readline(), 'utf-8').rstrip()
                     print(line, file=f)
                     #print(line)
+                    parse(line)
+
                 f.flush()
-                delay(read_delay)
+                led.toggle()
+                wfi()
+                #delay(read_delay)
         except KeyboardInterrupt:
             pass
 
+    led.off()
     os.sync()
+    LED(1).on()
 
 
 def run_gps_logger(uart_id, baudrate=9600):
