5c5542b018a6e111bf6a1e7e5e2126c8ae89abc6
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2016-12-06 03:41:08 +0200

    ssd1322: improved SSD1322 initialization

--- disp.py
+++ disp.py
@@ -3,7 +3,7 @@ from display import ssd1322
 
 
 def create_display():
-    spi = SPI(1)
+    spi = SPI(1, baudrate=3000000, polarity=0, phase=0)
     dc = Pin('Y2')
     res = Pin('Y1')
     cs = Pin('X5')
