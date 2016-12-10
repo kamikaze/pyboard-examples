17ddf87655e3741939119b8ef8d9ae5796744934
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2016-12-10 04:57:57 +0200

    ssd1322: working with SPI2 for now

--- disp.py
+++ disp.py
@@ -3,10 +3,10 @@ from display import ssd1322
 
 
 def create_display():
-    spi = SPI(1, baudrate=10*1024*1024, polarity=0, phase=0)
+    spi = SPI(2, baudrate=10*1024*1024, polarity=0, phase=0)
     dc = Pin('Y2')
     res = Pin('Y1')
-    cs = Pin('X5')
+    cs = Pin('Y5')
     display = ssd1322.SSD1322(spi, dc, cs, res, width=256, height=64)
 
     return display
