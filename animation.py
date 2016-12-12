90077f8a6c4a8934326ed6e74b3253e214398e21
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2016-12-12 14:35:07 +0200

    ssd1322: it looks like PyBoard SD is being underpowered and dies. One bad solution is to connect 3.3V display to V+ pin which is 5V when working from USB

--- animation.py
+++ animation.py
@@ -8,9 +8,14 @@ def read_frames(start, end, step=1, from_sd=False):
             yield f.read()
 
 
-def run_animation(display, start=0, end=10, step=1, delay=50, from_sd=False):
+def run_animation(display, start=0, end=10, step=1, delay=10, from_sd=False):
     while True:
         for frame in read_frames(start, end, step, from_sd):
+            pyb.delay(delay)
+            display.replace_buffer(frame)
+            display.send_buffer()
+
+        for frame in read_frames(end-1, start+1, -step, from_sd):
             pyb.delay(delay)
             display.replace_buffer(frame)
             display.send_buffer()
@@ -18,7 +23,7 @@ def run_animation(display, start=0, end=10, step=1, delay=50, from_sd=False):
 
 def run_test(from_sd=False):
     d = display.create_display()
-    run_animation(d, end=100, step=10, from_sd=from_sd)
+    run_animation(d, end=100, from_sd=from_sd)
 
 def run_test_from_flash():
     run_test()
--- display/__init__.py
+++ display/__init__.py
@@ -3,7 +3,7 @@ from display.ssd1322 import SSD1322
 
 
 def create_display():
-    spi = SPI(2, baudrate=10*1024*1024, polarity=0, phase=0)
+    spi = SPI(2, baudrate=20*1024*1024, polarity=0, phase=0)
     dc = Pin('Y2')
     res = Pin('Y1')
     cs = Pin('Y5')
