13c89e2d85f670fa3beeffadd51288bd6e35538f
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2016-12-13 02:51:37 +0200

    ssd1322: added all 248 frames for animation

--- animation.py
+++ animation.py
@@ -15,15 +15,9 @@ def run_animation(display, start=0, end=10, step=1, delay=10, from_sd=False):
             display.replace_buffer(frame)
             display.send_buffer()
 
-        for frame in read_frames(end-1, start+1, -step, from_sd):
-            pyb.delay(delay)
-            display.replace_buffer(frame)
-            display.send_buffer()
-
-
 def run_test(from_sd=False):
     d = display.create_display()
-    run_animation(d, end=100, from_sd=from_sd)
+    run_animation(d, end=248, from_sd=from_sd)
 
 def run_test_from_flash():
     run_test()
