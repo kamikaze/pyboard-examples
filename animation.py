44a0f00a6ed1be9241500a5a9eeff43a73a515e6
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2016-12-16 14:34:54 +0200

    ssd1322: frame load optimization

--- animation.py
+++ animation.py
@@ -2,22 +2,19 @@ import pyb
 import display
 
 
-def read_frames(start, end, step=1, from_sd=False):
-    for i in range(start, end, step):
-        with open('{0}/frames/anim_{1:05d}.gray'.format('/sd' if from_sd else '/flash', i), 'rb') as f:
-            yield f.read()
-
-
 def run_animation(display, start=0, end=10, step=1, delay=10, from_sd=False):
     while True:
-        for frame in read_frames(start, end, step, from_sd):
-            pyb.delay(delay)
-            display.replace_buffer(frame)
-            display.send_buffer()
+        for i in range(start, end, step):
+            with open('{0}/frames/anim_{1:05d}.gray'.format('/sd' if from_sd else '/flash', i), 'rb') as f:
+                f.readinto(display.buffer)
+                pyb.delay(delay)
+                display.send_buffer()
+
 
 def run_test(from_sd=False):
     d = display.create_display()
     run_animation(d, end=248, from_sd=from_sd)
+
 
 def run_test_from_flash():
     run_test()
