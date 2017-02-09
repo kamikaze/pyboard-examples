1e1bacd17a1448c4a60e3ff0bd26ee2f3479a83a
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2017-02-09 03:27:31 +0200

    uav: added pitch,roll,yaw params, introduced screen page renderers

--- uav.py
+++ uav.py
@@ -1,7 +1,7 @@
 import struct
 from math import atan2, pi
 
-from pyb import delay, RTC, USB_VCP
+from pyb import delay, Switch, RTC, USB_VCP
 from machine import I2C
 
 import display
@@ -9,9 +9,16 @@ from display.ssd1322 import SSD1322
 from imu.lsm303 import LSM303D
 
 
+SIGNALS = bytearray(1)
+SIGNAL_USR = 0b1
+
 rtc = RTC()
 uav = {
     'imu': {
+        'north': .0,
+        'roll': .0,
+        'pitch': .0,
+        'yaw': .0
     },
     'pos': {
         'alt': .0,
@@ -22,14 +29,15 @@ uav = {
         'hdg': .0
     }
 }
-screens = {
-    
-}
 
 
 def process_telemetry(sentence, data, checksum):
     if sentence == '$EXINJ':
+        imu = uav['imu']
         uav['pos']['hdg'] = float(data[0])
+        imu['roll'] = float(data[1])
+        imu['pitch'] = float(data[2])
+        imu['yaw'] = float(data[3])
 
 
 def update_gps_data(line):
@@ -60,7 +68,41 @@ def update_gps_data(line):
         process_telemetry(sentence, data, checksum)
 
 
+def render_gps_screen(framebuf, uav):
+    north = uav['imu']['north']
+    pos = uav['pos']
+    w = int(north / 2)
+
+    framebuf.fill(0)
+    framebuf.text('N: {}'.format(north), 0, 0, 0xF)
+    framebuf.text('HDG: {:6.3f}'.format(pos['hdg']), 0, 8, 0xF)
+    framebuf.text('ALT: {:6.1f} M'.format(pos['alt']), 0, 16, 0xF)
+    framebuf.text('LAT: {:10.6f} {}'.format(pos['lat'], pos['lat_part']), 0, 24, 0xF)
+    framebuf.text('LON: {:10.6f} {}'.format(pos['lon'], pos['lon_part']), 0, 32, 0xF)
+    framebuf.text('TIME: {}'.format(' '.join(map(str, rtc.datetime()))), 0, 40, 0xF)
+    framebuf.fill_rect(127 if w > 0 else 127 + w, 56, abs(w), 8, 0xF)
+
+
+def render_hud_screen(framebuf, uav):
+    imu = uav['imu']
+    framebuf.fill(0)
+    framebuf.text('ROLL: {}'.format(imu['roll']), 0, 0, 0xF)
+    framebuf.text('PITCH: {}'.format(imu['pitch']), 0, 8, 0xF)
+    framebuf.text('YAW: {}'.format(imu['yaw']), 0, 16, 0xF)
+
+
+def switch_cb():
+    global SIGNALS
+    global SIGNAL_USR
+
+    SIGNALS[0] = SIGNALS[0] | SIGNAL_USR
+
+
 def run_uav_test(i2c_bus=2):
+    global SIGNALS
+    global SIGNAL_USR
+
+    SIGNALS[0] = 0
     serial_port = USB_VCP()
     nmea_line = None
     #serial_port.setinterrupt(-1)
@@ -68,7 +110,14 @@ def run_uav_test(i2c_bus=2):
     i2c = I2C(i2c_bus, freq=400000)
     devices = i2c.scan()
     lsm303 = LSM303D(i2c)
+    switch = Switch()
     w = 0
+
+    screen_renderers = [render_gps_screen, render_hud_screen]
+    renderer_idx = 0
+    render_screen = screen_renderers[renderer_idx]
+
+    switch.callback(switch_cb)
 
     while True:
         # retrieving data
@@ -80,25 +129,18 @@ def run_uav_test(i2c_bus=2):
         # processing data
         x, y, z = accel
         x, z, y = mag
-        h = atan2(y, x) * 180.0 / pi
+        uav['imu']['north'] = atan2(y, x) * 180.0 / pi
 
         if nmea_line:
             update_gps_data(nmea_line)
             nmea_line = None
 
-        # displaying data
-        d.framebuf.fill(0)
-        d.framebuf.text('N: {}'.format(h), 0, 0, 0xF)
-        pos = uav['pos']
-        d.framebuf.text('HDG: {:6.3f}'.format(pos['hdg']), 0, 8, 0xF)
-        d.framebuf.text('ALT: {:6.1f} M'.format(pos['alt']), 0, 16, 0xF)
-        d.framebuf.text('LAT: {:10.6f} {}'.format(pos['lat'], pos['lat_part']), 0, 24, 0xF)
-        d.framebuf.text('LON: {:10.6f} {}'.format(pos['lon'], pos['lon_part']), 0, 32, 0xF)
-        d.framebuf.text('TIME: {}'.format(' '.join(map(str, rtc.datetime()))), 0, 40, 0xF)
+        if SIGNALS[0] & SIGNAL_USR:
+            renderer_idx = renderer_idx + 1
+            render_screen = screen_renderers[renderer_idx % len(screen_renderers)]
+            SIGNALS[0] = SIGNALS[0] & ~SIGNAL_USR
 
-        w = int(h / 2)
-
-        d.framebuf.fill_rect(127 if w > 0 else 127 + w, 56, abs(w), 8, 0xF)
+        render_screen(d.framebuf, uav)
 
         d.send_buffer()
         delay(50)
